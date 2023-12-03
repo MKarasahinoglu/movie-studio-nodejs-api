5fe121f6cca5dea6fc59e327bd332c95d8116117
Metehan Karaşahinoğlu <58992140+MKarasahinoglu@users.noreply.github.com>
2023-12-03 20:57:27 +0300

    Remove unnecessary code

--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 const express=require("express")
 const cookieParser=require("cookie-parser")
 const { config } = require("dotenv")
-const { verify } = require("jsonwebtoken")
 require("dotenv").config()
 
 const app=express()
